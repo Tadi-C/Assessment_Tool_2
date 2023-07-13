23e2dcd3e0c2bd1f24625633c131b60fc2b01d83
Tadi-C <tadichingwe0@gmail.com>
2023-07-13 14:58:22 +0200

    Removed dbmethods

--- Cheita VB/DbMethods.vb
+++
@@ -1,5 +0,0 @@
-﻿Public Class DbMethods
-    Public Shared connectionString As String = "Data Source=TADI-C;Initial Catalog=ChietaDemo;Integrated Security=True"
-    Public Shared paperNumber As String = String.Empty
-
-End Class
